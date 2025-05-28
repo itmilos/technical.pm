f4ea0fb67ca5afcf67e6fbecf019765dcf24708b
itmilos <mikeruje@gmail.com>
2025-05-28 19:14:23 -0400

    add node for server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
 import react from '@astrojs/react';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,6 +10,9 @@ export default defineConfig({
   site: 'https://technical.pm',
   compressHTML: true,
   output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
   build: {
     inlineStylesheets: 'auto'
   }
